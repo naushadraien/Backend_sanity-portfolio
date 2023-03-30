eb5fed2d8593963e1ba470165f1a9fe517a9608e
naushadraien <naushadraien5@gmail.com>
2023-03-30 22:42:46 +0545

    added cors

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,6 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '2bevx7gk',
-    dataset: 'production'
+    dataset: 'production',
+    cors: {
+      origin: ['https://www.naushadraien.com.np'],
+      credentials: true
+    }
   }
 })
